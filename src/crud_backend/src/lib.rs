31b63d73478e4995879680b9a1fc769f38250cf0
Chaitu-Tatipamula <chaitutatipamula023@gmail.com>
2024-07-14 20:40:50 +0530

    using stable structure istead of ic_cdk::storage

--- src/crud_backend/src/lib.rs
+++ src/crud_backend/src/lib.rs
@@ -1,86 +1,104 @@
-use ic_cdk::{init, query, storage, update};
+use ic_cdk::{init, query, update};
 use candid::{Nat, CandidType, Deserialize};
+use std::cell::RefCell;
+use std::collections::HashMap;
 
 #[derive(Default, CandidType, Deserialize, Clone)]
-struct Todo{
-    id : Nat,
-    desc : String,
-    completed : bool
+struct Todo {
+    id: Nat,
+    desc: String,
+    completed: bool,
 }
 
-#[derive(Default, CandidType, Deserialize)]
-struct TodoList {
-    items: Vec<Todo>,
+type TodoId = Nat;
+
+thread_local! {
+    static TODOS: RefCell<HashMap<TodoId, Todo>> = RefCell::new(HashMap::new());
+    static LAST_ID: RefCell<TodoId> = RefCell::new(Nat::from(0u64));
 }
 
 #[init]
 fn init() {
-    let initial_state = TodoList::default();
-    storage::stable_save((initial_state,)).unwrap();
-}
-
-#[update]
-fn create_todo(desc : String)-> Nat{
-    let mut todo_list: TodoList = storage::stable_restore::<(TodoList,)>().unwrap().0;
-    let id = Nat::from(todo_list.items.len() as u64 + 1);
-    todo_list.items.push(Todo{
-        id : id.clone(),
-        desc,
-        completed : false
+    TODOS.with(|todos| {
+        *todos.borrow_mut() = HashMap::new();
     });
-    storage::stable_save((todo_list,)).unwrap();
-    id
+    LAST_ID.with(|last_id| {
+        *last_id.borrow_mut() = Nat::from(0u64);
+    });
 }
 
 #[update]
-fn update_todo(id : Nat, new_desc : Option<String>, new_completed : Option<bool>)-> String {
-    let mut todo_list: TodoList = storage::stable_restore::<(TodoList,)>().unwrap().0;
-    if let Some(item) = todo_list.items.iter_mut().find(|item| item.id == id) {
-        if let Some(description) = new_desc {
-            item.desc = description;
-        }
-        if let Some(completed) = new_completed {
-            item.completed = completed;
-        }
-        storage::stable_save((todo_list,)).unwrap();
-        "Todo item updated successfully".to_string()    
-    }
-    else {
-        "Todo item not found".to_string()
-    }
-}
-
-#[query]
-fn get_todos()-> Vec<Todo>{
-    let todo_list: TodoList = storage::stable_restore::<(TodoList,)>().unwrap().0;
-    todo_list.items.clone()
-}
-
-#[query]
-fn get_paginated_todos(page_number : usize, page_size : usize)-> Vec<Todo>{
-    let todo_list: TodoList = storage::stable_restore::<(TodoList,)>().unwrap().0;
-    let start = page_number*page_size;
-    let end = (start + page_size).min(todo_list.items.len());
-    todo_list.items[start..end].to_vec()
-}
-#[query]
-fn get_latest_todos()-> Vec<Todo>{
-    let mut todo_list: TodoList = storage::stable_restore::<(TodoList,)>().unwrap().0;
-    todo_list.items.sort_by_key(|item| item.id.clone());
-    todo_list.items.into_iter().rev().take(10).collect()
+fn create_todo(desc: String) -> Nat {
+    TODOS.with(|todos| {
+        LAST_ID.with(|last_id| {
+            let mut last_id = last_id.borrow_mut();
+            *last_id += Nat::from(1u64);
+            let id = last_id.clone();
+            todos.borrow_mut().insert(id.clone(), Todo {
+                id: id.clone(),
+                desc,
+                completed: false,
+            });
+            id
+        })
+    })
 }
 
 #[update]
-fn delete_todo(id : Nat)-> String{
-    let mut todo_list: TodoList = storage::stable_restore::<(TodoList,)>().unwrap().0;
-    if let Some(pos) = todo_list.items.iter().position(|item| item.id == id){
-        todo_list.items.remove(pos);
-        storage::stable_save((todo_list,)).unwrap();
-        "Todo item updated successfully".to_string()    
-    }
-    else {
-        "Todo item not found".to_string()
-    }
+fn update_todo(id: Nat, new_desc: Option<String>, new_completed: Option<bool>) -> String {
+    TODOS.with(|todos| {
+        let mut todos = todos.borrow_mut();
+        if let Some(item) = todos.get_mut(&id) {
+            if let Some(description) = new_desc {
+                item.desc = description;
+            }
+            if let Some(completed) = new_completed {
+                item.completed = completed;
+            }
+            "Todo item updated successfully".to_string()
+        } else {
+            "Todo item not found".to_string()
+        }
+    })
+}
+
+#[query]
+fn get_todos() -> Vec<Todo> {
+    TODOS.with(|todos| {
+        let todos = todos.borrow();
+        todos.values().cloned().collect()
+    })
+}
+
+#[query]
+fn get_paginated_todos(page_number: usize, page_size: usize) -> Vec<Todo> {
+    TODOS.with(|todos| {
+        let todos = todos.borrow();
+        let start = page_number * page_size;
+        let end = (start + page_size).min(todos.len());
+        todos.values().skip(start).take(end - start).cloned().collect()
+    })
+}
+
+#[query]
+fn get_latest_todos() -> Vec<Todo> {
+    TODOS.with(|todos| {
+        let mut todos = todos.borrow().values().cloned().collect::<Vec<_>>();
+        todos.sort_by_key(|item| item.id.clone());
+        todos.into_iter().rev().take(10).collect()
+    })
+}
+
+#[update]
+fn delete_todo(id: Nat) -> String {
+    TODOS.with(|todos| {
+        let mut todos = todos.borrow_mut();
+        if todos.remove(&id).is_some() {
+            "Todo item deleted successfully".to_string()
+        } else {
+            "Todo item not found".to_string()
+        }
+    })
 }
 
 ic_cdk::export_candid!();
